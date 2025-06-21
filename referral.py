fa40c1fe626759e2c59b7cb0af43e9d09fab78f3
Anik-Star-Max <debjitdas242@gmail.com>
2025-06-21 23:29:27 +0530

    Delete referral.py

--- referral.py
+++
@@ -1,7 +0,0 @@
-import config
-
-def generate_referral_link(user_id):
-    return f"https://t.me/{config.BOT_USERNAME}?start=ref_{user_id}"
-
-def is_valid_referral(referral_code):
-    return referral_code.startswith('ref_') and referral_code.split('_')[1].isdigit()
