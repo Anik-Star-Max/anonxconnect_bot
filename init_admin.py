47104f3b2cf1167ebae0866bdc962c98c1910b91
Anik-Star-Max <debjitdas242@gmail.com>
2025-06-22 15:30:01 +0530

    init_admin.py

--- init_admin.py
+++ init_admin.py
@@ -4,6 +4,6 @@ from config import ADMIN_ID
 # Ensure admin always has VIP and is unbanned
 admin = database.get_user(ADMIN_ID)
 if not admin:
-    database.create_user(ADMIN_ID, "@mysteryman02")
+    database.create_user(ADMIN_ID, None)  
 database.give_vip(ADMIN_ID, 3650)
 database.unban_user(ADMIN_ID)
