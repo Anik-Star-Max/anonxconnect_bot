eedc45496326788f89dff913d10f1958159d89cd
Anik-Star-Max <debjitdas242@gmail.com>
2025-06-23 16:46:17 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
-BOT_USERNAME = os.getenv("BOT_USERNAME")
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+ADMIN_ID = os.environ["ADMIN_ID"]
+BOT_USERNAME = os.environ["BOT_USERNAME"]
